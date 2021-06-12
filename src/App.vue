ba153b650501dab4fd965ff358defbe634418144
sachetsingh77 <sachet@leverj.io>
2021-06-12 10:37:15 +0530

    updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <BinaryOptions msg="Gluon V2 Binary Options"/>
+    <BinaryOptions msg="Binary Options"/>
   </div>
 </template>
 
